dfbe06b22dc579d4e41da04d670de43fb54c2f10
Novan Junaedi <junaedinovan98@gmail.com>
2020-07-09 21:13:05 +0700

    adding detail-destination

--- resources/views/main/detail_wisata.blade.php
+++ resources/views/main/detail_wisata.blade.php
@@ -8,14 +8,24 @@
 
 @section('content')
 <div class="container">
-    <h1>List Wisata Di (replace nama Lokasi) </h1>
     <div class="row">
-        <div class="col-md-3 ftco-animate mb-4">
-            <div class="banner" style="width: 600px; height:400px;">
-                <a width=256px height=256px href="#" class="img" style="background-image: url(http://placehold.it/256x256);"> halo
-                </a>
-            </div>
+        <div class="col-lg-12 ftco-animate mb-4 mt-4">
+        	<div class="card shadow p-3 mb-5 bg-white rounded">
+        		<div class="container">
+        			<h1 class="card-title">Taman Mini Indonesia Indah</h1>
+        			<h5><i class="fas fa-fw fa-map-marker"></i> Jakarta | <i class="fas fa-fw fa-star"></i> 5 </h5>
+        			<center>
+        				<img class="thumb-img" src="{{asset('/images/tmii.jpg')}}" alt="gambar">
+        			</center>
+        			<h5 class="card-content mt-4">Deskripsi</h5>
+        			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ducimus dolor praesentium minima in nisi unde? Eos hic rem tempora labore et harum, soluta natus repudiandae, repellat ratione ad quasi!</p>
+        			<h5 class="card-content mt-4">Harga Tiket</h5>
+        			<p>Rp.50000 /orang.</p>
+        			<a class="btn btn-warning mt-4 mb-4" href="#">Pesan Tiket Sekarang</a>
+        		</div>
+        	</div>
         </div>
     </div>
+ </div>
 </section>
 @endsection
