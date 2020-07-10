2386bb4549343503374e4e5a2f08e1455d88319e
Novan Junaedi <junaedinovan98@gmail.com>
2020-07-10 15:52:53 +0700

    detail-list

--- resources/views/main/detail_list.blade.php
+++ resources/views/main/detail_list.blade.php
@@ -12,14 +12,23 @@
     {{-- <h1>List Wisata Di {{$bdgall->id}} </h1> --}}
     <div class="row">
         @foreach($allget as $list)
-        <div class="col-md-3 ftco-animate mb-4">
-            <div class="project-destination">
-                <a href="#" class="img" style="background-image: url(http://placehold.it/256x256);">
-                    <div class="text">
-                    <h3>Harga {{$list->price}}</h3>
-                        <span>{{$list->destination_name}}</span>
-                    </div>
-                </a>
+        <div class="col-lg-12 ftco-animate mb-4">
+            <div class="card shadow p-3 mb-3 bg-white rounded">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="{{$list->img}}" class="card-img" alt="location">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title"><a href="/detail-wisata/{{$list->id}}">{{$list->destination_name}}</a></h5>
+                    <p class="card-text">
+                        Lokasi : {{$list->location}} <br />
+                        Harga Tiket : Rp. {{$list->price}} -, / orang.
+                    </p>
+                    <p class="card-text"><small class="text-muted"><a href="/detail-wisata/{{$list->id}}">Lihat selengkapnya</a></small></p>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
         @endforeach
