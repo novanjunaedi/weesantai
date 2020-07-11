d109ccf2b738eb62f31b0164e23a245f79233384
ryzkymd <ryzkysam@gmail.com>
2020-07-11 09:30:22 +0700

    fix middleware dan menambahkan profile user dinavbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -46,8 +46,40 @@
                     <li class="nav-item {{ Request::is('destination') ? 'active' : '' }}"><a href="/destination" class="nav-link">Destinasi</a></li>
                     <li class="nav-item {{ Request::is('about') ? 'active' : '' }}"><a href="/about" class="nav-link">Tentang</a></li>
                     <li class="nav-item {{ Request::is('contact') ? 'active' : '' }}"><a href="/contact" class="nav-link">Kontak</a></li>
+                    @php $user = Auth::guard('role')->user() @endphp
+                    @if ($user)
+                    @if ($user->role == 'user')
+                    <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="mr-2 d-none d-lg-inline text-gray-600">{{$user->name}}</span>
+                            <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                        </a>
+                        <!-- Dropdown - User Information -->
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Profile
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Settings
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Activity Log
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="/logout" data-toggle="modal" data-target="#logoutModal">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
+                            </a>
+                        </div>
+                    </li>
+                    @endif
+                    @else
                     <li class="nav-item"><a href="/login" class="nav-link">Masuk</a></li>
                     <li class="nav-item"><a href="/register" class="nav-link">Daftar</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
@@ -91,13 +123,29 @@
         </div>
     </footer>
 
-
-
     <!-- loader -->
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
             <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
             <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
 
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="/logout">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="{{asset('js/main/jquery.min.js')}}"></script>
     <script src="{{asset('js/main/jquery-migrate-3.0.1.min.js')}}"></script>
--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware(['auth:role', 'checkRole:user,admin'])->group(function () {
-    Route::get('/', 'MainController@index');
-    Route::get('/destination', 'MainController@destination');
-    Route::get('/about', 'MainController@about');
-    Route::get('/contact', 'MainController@contact');
-});
+Route::get('/', 'MainController@index');
+Route::get('/destination', 'MainController@destination');
+Route::get('/about', 'MainController@about');
+Route::get('/contact', 'MainController@contact');
 
 //route detail-list
-Route::middleware(['auth:role', 'checkRole:user,admin'])->group(function () {
-    Route::get('/detail-list/Bandung', 'ListController@get_bandung');
-    Route::get('/detail-list/Jakarta', 'ListController@get_jakarta');
-    Route::get('/detail-list/Bali', 'ListController@get_bali');
-    Route::get('/detail-list/Yogyakarta', 'ListController@get_yogyakarta');
-    Route::get('/detail-list/Malang', 'ListController@get_malang');
-    Route::get('/detail-list/Surabaya', 'ListController@get_surabaya');
-    Route::get('/detail-list/Lombok', 'ListController@get_lombok');
-    Route::get('/detail-list/Lampung', 'ListController@get_lampung');
-    Route::get('/detail-list', 'ListController@get_back');
-    Route::get('/detail-wisata/{destination_name}', 'MainController@detail_wisata');
-});
+Route::get('/detail-list/Bandung', 'ListController@get_bandung');
+Route::get('/detail-list/Jakarta', 'ListController@get_jakarta');
+Route::get('/detail-list/Bali', 'ListController@get_bali');
+Route::get('/detail-list/Yogyakarta', 'ListController@get_yogyakarta');
+Route::get('/detail-list/Malang', 'ListController@get_malang');
+Route::get('/detail-list/Surabaya', 'ListController@get_surabaya');
+Route::get('/detail-list/Lombok', 'ListController@get_lombok');
+Route::get('/detail-list/Lampung', 'ListController@get_lampung');
+Route::get('/detail-list', 'ListController@get_back');
+Route::get('/detail-wisata/{destination_name}', 'MainController@detail_wisata');
 
 // Auth
 Route::middleware(['guest:role'])->group(function () {
@@ -39,15 +35,15 @@ Route::middleware(['guest:role'])->group(function () {
     Route::post('/register', 'AuthController@postRegister');
 });
 
-Route::get('/logout', 'AuthController@logout');
+Route::get('/logout', 'AuthController@logout')->middleware('auth:role');
 
 Route::middleware(['auth:role', 'checkRole:user,admin'])->group(function () {
     Route::get('/e-ticket', 'TicketController@index');
     Route::get('/e-ticket/pdf', 'TicketController@pdf');
 });
 
+Route::get('/dashboard', 'DashboardController@index');
 Route::middleware(['auth:role', 'checkRole:admin'])->group(function () {
-    Route::get('/dashboard', 'DashboardController@index');
     Route::get('/add-destination', 'DestinationController@index');
     Route::post('/add-destination/create', 'DestinationController@create');
     Route::get('/add-destination/{id}/edit', 'DestinationController@edit');
