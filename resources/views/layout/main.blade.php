9275db90e0aa1bd6dc5ad33f406e009092e2d7ec
Fajar Mukti Hidayat <fajarmukti9@gmail.com>
2020-07-12 13:29:10 +0700

    img navbar condition

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -53,7 +53,11 @@
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600">{{$user->name}}</span>
-                            <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                            @if ($user->img==null)
+                            <img class="img-profile rounded-circle" src="{{ asset('profile_default/default-pria.png')}}">
+                            @else
+                            <img class="img-profile rounded-circle" src="{{$user->img}}">    
+                            @endif
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
