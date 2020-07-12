5475dde623b948f3ff80862ff1abecca984f6aec
Novan Junaedi <junaedinovan98@gmail.com>
2020-07-12 20:49:16 +0700

    fixing layout

--- resources/views/user/userprofile.blade.php
+++ resources/views/user/userprofile.blade.php
@@ -20,11 +20,11 @@
     <div class="container">
         <div class="row">
         	<div class="col-lg-4 mb-2">
-        		<div class="card">
+        		<div class="card shadow p-3 mb-5 bg-white rounded">
 					@if ($user_data->img == null )
 					<img src="{{ asset('profile_default/default-pria.png') }}" height="300px" class="card-img-top p-3 img-fluid" alt="profile">
 					@else
-				  	<img src="{{ asset('user_img/'.$user_data->img) }}" class="card-img-top p-3 img-fluid" alt="profile">
+				  	<img src="{{ asset('user_img/'.$user_data->img) }}" class="card-img-top p-3 img-fluid img-profile rounded-circle" alt="profile">
 				  	@endif
 				  <div class="card-body">
 				    <h5 class="card-title">{{$user_data->name}}</h5>
@@ -61,10 +61,85 @@
 				</div>
         	</div>
         	<div class="col-lg-8">
-        		<div class="card" style="min-height: 150vh;">
+        		<div class="card shadow p-3 mb-5 bg-white rounded" style="min-height: 150vh;">
 				  <div class="card-body">
 				    <h2 class="card-title">Dashboard</h2>
-				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <h5 class="card-text pt-3">
+				    	Histori Transaksi
+				    </h5>
+				    <div class="row">
+				    	<div class="col">
+				    		<div class="card">
+				    			<p class="card-title p-3">Taman Mini Indonesia Indah</p>
+				    		</div>
+				    	</div>	
+				    </div>
+				    <div class="row">
+				    	<div class="col">
+				    		<div class="card">
+				    			<p class="card-title p-3">Taman Mini Indonesia Indah</p>
+				    		</div>
+				    	</div>	
+				    </div>
+				    <div class="row">
+				    	<div class="col">
+				    		<div class="card">
+				    			<p class="card-title p-3">Taman Mini Indonesia Indah</p>
+				    		</div>
+				    	</div>	
+				    </div>
+				    <div class="row">
+				    	<div class="col">
+				    		<div class="card">
+				    			<p class="card-title p-3">Taman Mini Indonesia Indah</p>
+				    		</div>
+				    	</div>	
+				    </div>
+				    <div class="row">
+				    	<div class="col">
+				    		<div class="card">
+				    			<p class="card-title p-3">Taman Mini Indonesia Indah</p>
+				    		</div>
+				    	</div>	
+				    </div>
+
+				    <h5 class="card-text pt-4">
+				    	Rekomendasi Untuk Anda
+				    	<div class="row pt-4">
+				    		<div class="col-md-6 ftco-animate">
+								<div class="project-wrap">
+									<a href="#" class="img" style="background-image: url({{asset($user_data->img)}});"></a>
+									<div class="text p-4">
+										<span class="price">Rp. harga</span>
+										<span class="pt-3">
+											<a href="#">lokasi</a>
+										</span>
+										<p class="card-text pt-3">
+											<i class="fas fa-fw fa-map-marker"></i>alamat
+										</p>
+										<p style="text-align: right;"><a class="card-link" href="#">Detail</a></p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-md-6 ftco-animate">
+								<div class="project-wrap">
+									<a href="#" class="img" style="background-image: url({{asset($user_data->img)}});"></a>
+									<div class="text p-4">
+										<span class="price">Rp. harga</span>
+										<span class="pt-3">
+											<a href="#">lokasi</a>
+										</span>
+										<p class="card-text pt-3">
+											<i class="fas fa-fw fa-map-marker"></i>alamat
+										</p>
+										<p style="text-align: right;"><a class="card-link" href="#">Detail</a></p>
+									</div>
+								</div>
+							</div>
+
+				    	</div>
+				    </h5>
 				  </div>
 				</div>
         	</div>
