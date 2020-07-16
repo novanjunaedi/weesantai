c81b65286d991234d9da250db051890d1c04bb56
Novan Junaedi <junaedinovan98@gmail.com>
2020-07-17 00:18:40 +0700

    improve layout detail list

--- resources/views/main/detail_list.blade.php
+++ resources/views/main/detail_list.blade.php
@@ -4,13 +4,13 @@
 
 @section('bg-banner', 'background-image: url("/img/main/banner.jpg");')
 
-@section('title-banner', 'Temukan Tempat Favorit Anda')
+@section('title-banner')
+<div class="title-banner">
+    Tempat wisata di {{$loc}}
+</div>
+@endsection
 
 @section('content')
-
-<div class="title-wisata text-center" style="background-color: orange; padding: 25px;">
-    <h1>Tempat wisata di {{$loc}}</h1>
-</div>
 
 <section class="ftco-section">
     <div class="container">
@@ -21,14 +21,14 @@
                     <div class="row no-gutters">
                         <div class="col-md-4">
                             <img src="{{asset($list->img)}}" class="card-img" alt="location" height="200px" width="800px">
-                            @php dd($list->img); @endphp
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title"><a href="/detail-wisata/{{$list->destination_name}}">{{$list->destination_name}}</a></h5>
                                 <p class="card-text">
-                                    Lokasi : {{$list->location}} <br />
-                                    Harga Tiket : Rp. {{$list->price}} -, / orang.
+                                    <i class="fas fa-fw fa-star"></i> Rating : {{$list->rating}} <br />
+                                    <i class="fas fa-fw fa-money-bill-alt"></i> Tiket : Rp. {{$list->price}} -, / orang. <br />
+                                    <i class="fas fa-fw fa-map-marker"></i> Alamat : {{$list->address}} <br />
                                 </p>
                                 <p class="card-text"><small class="text-muted"><a href="/detail-wisata/{{$list->destination_name}}">Lihat selengkapnya</a></small></p>
                             </div>
